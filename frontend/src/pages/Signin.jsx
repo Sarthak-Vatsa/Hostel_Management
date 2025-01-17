0cda01656d2c8c802a1fa93a46f2260d3b6bd4d4
gaurav <gaurav@gauravs-MacBook-Air-2.local>
2025-01-17 17:56:01 +0530

    Complete Homepage and signUp page

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <div className='bg-yellow-200 flex h-screen justify-center'>
+        <div className='h-full flex flex-col justify-center max-w-3xl w-full items-center'>
+            <div className='border-black border-2 h-min flex flex-col items-center p-4 space-y-8 max-w-xl w-full bg-white shadow-2xl rounded-lg'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signin
